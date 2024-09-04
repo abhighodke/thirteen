d94c9bd506f5c2271b2ee4ed76ab55ee0339ff75
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:38:26 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "SJ",
-    heroTagline: "Savannah Johnson Realty",
-    agentName: "Savannah Johnson",
+    headerInitials: "BF",
+    heroTagline: "Brody Frost Realty",
+    agentName: "Brody Frost",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Savannah Johnson was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Savannah Johnson provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Savannah Johnson were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Savannah Johnson and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Savannah Johnson is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Brody Frost was a game-changer for us. Him deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Brody Frost provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Brody Frost were evident throughout our home-buying journey. Him ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Brody Frost and Him team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Brody Frost is the one. Him dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "savannahsellsbham@gmail.com",
-    address: "AL, USA",
-    phone: "256-293-0675",
-    footerDescription: `Savannah Johnson Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "brodyfrost@kw.com",
+    address: "219 Main Street \nTrussville, AL 35173",
+    phone: "205-603-5734",
+    footerDescription: `Brody Frost Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/savannah.l.johnson.1",
+        facebook: "https://www.facebook.com/brodyfrost.RE/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "savannahsellsbham@gmail.com",
-        phone: "256-293-0675",
+        email: "brodyfrost@kw.com",
+        phone: "205-603-5734",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/447461131_7906357032720261_5843336229759713803_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XpFz5dK5MDcQ7kNvgEArdNf&_nc_ht=scontent-sjc3-1.xx&oh=00_AYB2F4QgIorv1rE8MWLJjN6cAjN4siBDDXYlqvn4eO-VQQ&oe=66D7CB15" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/391621163_1739490253233449_8017462229210845707_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=foYM6UCpz9wQ7kNvgEu1gT0&_nc_ht=scontent-sjc3-1.xx&oh=00_AYCQ4yJs6woYJkkan4pmfaiK3dc_xjT8XuBzigVcRmsz8A&oe=66DBBBEF" // Agent image URL
 };
 
 export default clientData;
